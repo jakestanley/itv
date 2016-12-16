bf1cb66ff6cccfe8fe6a7b99ec89da74e20ba5aa
Jake Stanley <prudentia713@gmail.com>
2016-12-16 12:22:22 +0000

    added Offer class and added Lombok to pom.xml

---
+++ src/main/java/uk/co/jakestanley/Offer.java
@@ -0,0 +1,12 @@
+package uk.co.jakestanley;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter @AllArgsConstructor
+class Offer {
+
+    private int quantity;
+    private int price;
+
+}
